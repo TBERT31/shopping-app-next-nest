fbdafee3b69741a8af1cb6941fbe8f0bcc443ac2
TBERT31 <berteau.thomas@hotmail.fr>
2025-03-21 08:50:45 +0100

    Set Primary color and prepare login page

--- poc-ui/app/layout.tsx
+++ poc-ui/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import "./globals.css";
+import { Container, CssBaseline, ThemeProvider } from "@mui/material";
+import lightTheme from "./styles/light.theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Container>
+              {children}
+            </Container>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
