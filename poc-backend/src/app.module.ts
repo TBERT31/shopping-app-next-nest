5109aa3fb3bb4f84d959c429e93ccabd4c8133bb
TBERT31 <berteau.thomas@hotmail.fr>
2025-03-30 11:55:40 +0200

    Prisma init

--- poc-backend/src/app.module.ts
+++ poc-backend/src/app.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production'
+
+        return {
+          pinoHttp: {
+            transport: isProduction 
+              ? undefined 
+              : {
+                target: 'pino-pretty',
+                options: {
+                  singleLine: true,
+                },
+              },
+            level: isProduction ? 'info' : 'debug',
+          }
+        }
+      },
+      inject: [ConfigService],
+    }),
     ConfigModule.forRoot(),
     UsersModule
   ],
