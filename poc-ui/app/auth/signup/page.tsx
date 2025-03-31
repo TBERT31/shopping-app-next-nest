2b3627ae74bd27dfe80dd127171cdfee90183bdd
TBERT31 <berteau.thomas@hotmail.fr>
2025-03-31 21:12:44 +0200

    createUser server request on frontend

--- poc-ui/app/auth/signup/page.tsx
+++ poc-ui/app/auth/signup/page.tsx
@@ -1,17 +1,31 @@
+"use client";
+
 import React from 'react'
 import { Button, Link, Stack, TextField } from '@mui/material'
 import NextLink from 'next/link'
+import { useFormState } from 'react-dom';
+import createUser from './create-user';
 
 export default function SignUp() {
+  const [state, formAction] = useFormState(createUser, {error: ""});
+
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-        <TextField label="Email" variant="outlined" type="email" />
-        <TextField label="Password" variant="outlined" type="password" />
-        <Button variant="contained" color="primary">Signup</Button>
-        <Link component={NextLink} href="/auth/login" className='self-center'>
-            Login
-        </Link>
-    </Stack>
+    <form action="formAction" className="w-full max-w-xs">
+      <Stack spacing={2}>
+          <TextField label="Email" variant="outlined" type="email" />
+          <TextField label="Password" variant="outlined" type="password" />
+          <Button 
+            type="submit" 
+            variant="contained" 
+            color="primary"
+          >
+            Signup
+          </Button>
+          <Link component={NextLink} href="/auth/login" className='self-center'>
+              Login
+          </Link>
+      </Stack>
+    </form>
   )
 }
 
