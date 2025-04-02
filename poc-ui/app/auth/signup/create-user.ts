6bf2c50f4315707243c3119728df95c5f212b9b9
t.berteau <thomas.berteau@egis-group.com>
2025-04-02 10:43:32 +0200

    fix frontend send data

--- poc-ui/app/auth/signup/create-user.ts
+++ poc-ui/app/auth/signup/create-user.ts
@@ -5,13 +5,19 @@ import { getErrorMessage } from "@/app/util/errors";
 import { redirect } from "next/navigation";
 
 export default async function createUser(_prevState: any, formData: FormData) {
-  const res = await fetch(`${API_URL}/users`, {
-    method: "POST",
-    body: formData,
-  });
-  const parsedRes = await res.json();
-  if (!res.ok) {
-    return { error: getErrorMessage(parsedRes) };
-  }
-  redirect("/");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    const res = await fetch(`${API_URL}/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+    });
+    const parsedRes = await res.json();
+    if (!res.ok) {
+        return { error: getErrorMessage(parsedRes) };
+    }
+    redirect("/");
 }
