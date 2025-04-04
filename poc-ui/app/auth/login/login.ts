4bf2f947a584b25e27f4055cb6973de1df2b68e4
t.berteau <thomas.berteau@egis-group.com>
2025-04-04 16:28:59 +0200

    auth/login formAction on frontend

---
+++ poc-ui/app/auth/login/login.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { FormError } from "@/app/common/form-error.interface";
+import { post } from "@/app/util/fetch";
+import { redirect } from "next/navigation";
+
+export default async function login(_prevState: FormError, formData: FormData) {
+    const { error } = await post("auth/login", formData);
+    if(error){
+        return { error };
+    }
+    redirect("/");
+}
--- poc-ui/app/auth/login/page.tsx
+++ poc-ui/app/auth/login/page.tsx
@@ -1,17 +1,50 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import { Button, Link, Stack, TextField } from '@mui/material'
 import NextLink from 'next/link'
+import { useFormState } from 'react-dom';
+import login from './login';
 
 export default function Login() {
+  const [state, formAction] = useFormState(login, {error: ""});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-        <TextField label="Email" variant="outlined" type="email" />
-        <TextField label="Password" variant="outlined" type="password" />
-        <Button variant="contained" color="primary">Login</Button>
-        <Link component={NextLink} href="/auth/signup" className='self-center'>
-            Signup
-        </Link>
-    </Stack>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+          <TextField 
+            name='email' 
+            label="Email" 
+            variant="outlined" 
+            type="email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            helperText={state.error}
+            error={!!state.error}
+          />
+          <TextField 
+            name='password' 
+            label="Password" 
+            variant="outlined" 
+            type="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            helperText={state.error}
+            error={!!state.error}
+          />
+          <Button 
+            type='submit' 
+            variant="contained" 
+            color="primary"
+          >
+            Login
+          </Button>
+          <Link component={NextLink} href="/auth/signup" className='self-center'>
+              Signup
+          </Link>
+      </Stack>
+    </form>
   )
 }
-//#A3BE07
