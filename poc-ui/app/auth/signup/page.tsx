6e2f51f7b9f504a11597fb651999c3986021ce62
t.berteau <thomas.berteau@egis-group.com>
2025-04-04 11:56:12 +0200

    refactor Nextjs post func

--- poc-ui/app/auth/signup/page.tsx
+++ poc-ui/app/auth/signup/page.tsx
@@ -4,9 +4,12 @@ import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
 import { useFormState } from "react-dom";
 import createUser from "./create-user";
+import { useState } from "react";
 
 export default function Signup() {
   const [state, formAction] = useFormState(createUser, { error: "" });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <form action={formAction} className="w-full max-w-xs">
@@ -16,6 +19,8 @@ export default function Signup() {
           label="Email"
           variant="outlined"
           type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           helperText={state.error}
           error={!!state.error}
         />
@@ -24,6 +29,8 @@ export default function Signup() {
           label="Password"
           variant="outlined"
           type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           helperText={state.error}
           error={!!state.error}
         />
