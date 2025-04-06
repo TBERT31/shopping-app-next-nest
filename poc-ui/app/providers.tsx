a27edfc78ef2fd51f6c571698813aba186dbfe3f
TBERT31 <berteau.thomas@hotmail.fr>
2025-04-06 15:38:21 +0200

    Authentication state manage with API Context

--- poc-ui/app/providers.tsx
+++ poc-ui/app/providers.tsx
@@ -2,16 +2,20 @@ import lightTheme from "./common/styles/light.theme";
 import { ThemeProvider } from "@mui/material";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ReactElement } from "react";
+import { AuthContext } from "./auth/auth-context";
 
 interface ProvidersProps {
     children: ReactElement[];
+    authenticated: boolean;
 }
 
-export default function Providers({children}: ProvidersProps) {
+export default function Providers({ children, authenticated }: ProvidersProps) {
     return (
          <AppRouterCacheProvider>
             <ThemeProvider theme={lightTheme}>
-                {children}
+                <AuthContext.Provider value={authenticated}>
+                    {children}
+                </AuthContext.Provider>
             </ThemeProvider>
          </AppRouterCacheProvider>
     )
