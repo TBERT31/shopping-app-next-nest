55876fc79bdf45d7a0e18a02895a60b273af0c81
TBERT31 <berteau.thomas@hotmail.fr>
2025-04-06 14:50:46 +0200

    Import Header from MUI (example)

--- poc-ui/app/layout.tsx
+++ poc-ui/app/layout.tsx
@@ -1,20 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import "./globals.css";
 import { Container, CssBaseline, ThemeProvider } from "@mui/material";
 import lightTheme from "./styles/light.theme";
-//import darkTheme from "./styles/dark.theme";
+import Header from "./components/header/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,15 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={lightTheme}>
             <CssBaseline />
-            <Container>
-              {children}
-            </Container>
+              <Header />
+              <Container> {children}</Container>
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
