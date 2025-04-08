05ecec42dec7533b9bba31096e1aeed003a61edc
t.berteau <thomas.berteau@egis-group.com>
2025-04-08 10:35:06 +0200

    Implement cache with revalidateTag on Nextjs

--- poc-ui/app/products/actions/create-product.ts
+++ poc-ui/app/products/actions/create-product.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import { revalidateTag } from "next/cache";
 import { post } from "../../common/util/fetch";
 
 export default async function createProduct(formData: FormData) {
     const { error } = await post("products", formData);
+    revalidateTag("products")
     return { error }; // WARNING DO NOT return `res`, as this will cause the client to crash.
 }
--- poc-ui/app/products/products.tsx
+++ poc-ui/app/products/products.tsx
@@ -8,7 +8,6 @@ export default async function Products() {
     return (
         <Grid container spacing={3}>
             {products.map(product => (
-                // <Grid key={product.id} sm={6} lg={4} xs={12}> // Old version of MUI
                 <Grid key={product.id} size={{sm:6, lg:4 , xs:12}}>
                     <Product product={product}/>
                 </Grid>
